5261c761b6be8636fd720084e9e9c0683770ce47
Evgenii Burikov <evgenii.burikov@gmail.com>
2021-07-23 16:33:06 +0300

    fix 10

--- src/Http/Controllers/RegionController.php
+++ src/Http/Controllers/RegionController.php
@@ -1,11 +1,13 @@
 <?php
 namespace Belivey\GeoAdmin\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Belivey\GeoAdmin\Models\Region;
 
 class RegionController extends Controller
 {
-    public function index () {
+    public function index (Request $request) {
         return Region::all();   
     }
 }
